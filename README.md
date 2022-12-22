ebe897baba742e4717fe5ec6c226b4c5088becb4
Madh00ra <102720622+Madh00ra@users.noreply.github.com>
2022-12-22 19:49:04 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,30 @@
+# QR-Code-generator-using-Python-and-Tkinter
+
+Nowadays we all use QR code for multiple purposes like making payments, sharing WiFi, etc. Have you ever wished to generate such QR code on your own? We are here with a project for QR code generator using Python. So, follow us to build this Python project.
+
+About QR Code Generator Project
+QR Code is a machine-readable matrix barcode that uniquely represents information. With the increase in optical capabilities of smartphones, the use of QR codes started increasing.
+
+In this project, we will build a QR Code generator using Python Modules.
+
+Generate QR Code in Python
+We will be using Tkinter and Qrcode modules in Python to build this project. We use the Tkinter module to build the GUI to take the following inputs:
+
+text/URL to convert to QR Code
+location to save the QR code with name
+size of the QR code.
+Then we generate the QR code from the above inputs and save in the given location using the qrcode module.
+
+Project Prerequisites
+It is suggested that the developer has prior knowledge on Python and the Tkinter module. Also, use the below commands to install the qrcode and tkinter modules.
+
+pip install qrcode
+Steps to build the QR Code Generator in Python
+To build the QR code generator project using Python we need to follow the below steps:
+1. Importing the modules
+2. Creating the main window
+3. Taking the input of the text/URL, location to store the QR code, name of the QR code and the size of the QR code
+4. Writing the function to generate and save the QR Code
+
+Summary
+Hurray! We have successfully built the QR Code generator in Python. In this, we learned the basic widgets of the tkinter module and to generate the QR Code using the qrcode module. Hope you enjoyed building this project.
---
+++ main.py
@@ -0,0 +1,46 @@
+import qrcode
+from tkinter import*
+from tkinter import messagebox
+root=Tk()
+root.title("QR Code Generator")
+root.geometry("700x700")
+root.config(bg="SteelBlue")
+
+def code():
+    qr = qrcode.QRCode(version=size.get(),box_size=10,border=5)
+    qr.add_data(text.get())
+    qr.make(fit=True)
+    img = qr.make_image()
+    img.save(f'.png')
+    messagebox.showinfo("QR Code Generator", "QR Code is successfully saved")
+frame1 = Frame(root, bg='azure', bd=8)
+frame1.place(relx=0.15, rely=0.05, relwidth=0.7, relheight=0.1)
+label1 = Label(frame1, text="QR Code Generator", bg='azure', font="Times 20 bold")
+label1.place(relx=0, rely=0, relwidth=1, relheight=1)
+
+#To take Input text
+frame2 = Frame(root,bg="SteelBlue")
+frame2.place(relx=0.1, rely=0.15, relwidth=0.7, relheight=0.3)
+label2 = Label(frame2,text="Enter the text/URL: ",bg="SteelBlue", fg='azure', font="Courier 13 bold")
+label2.place(relx=0.05, rely=0.2, relheight=0.08)
+text = Entry(frame2, font='Century 12')
+text.place(relx=0.05, rely=0.4, relwidth=1, relheight=0.2)
+
+#To take input image name
+frame3 = Frame(root, bg="SteelBlue")
+frame3.place(relx=0.1, rely=0.35, relwidth=0.7, relheight=0.3)
+label3 = Label(frame3, text="Enter the name of the QR Code", bg="SteelBlue", fg='azure', font="Courier 13 bold")
+label3.place(relx=0.05, rely=0.2, relheight=0.2)
+name = Entry(frame3, font="Century 12")
+name.place(relx=0.05, rely=0.4, relwidth=1, relheight=0.2)
+
+#To take input of the sie of QR code
+frame4 = Frame(root, bg="SteelBlue")
+frame4.place(relx=0.1, rely=0.55, relwidth=0.7, relheight=0.3)
+label4 = Label(frame4, text="Enter the size from 1 to 40:", bg="SteelBlue", fg='azure', font='Courier 13 bold')
+label4.place(relx=0.05, rely=0.2, relheight=0.08)
+size = Entry(frame4, font='Century 12')
+size.place(relx=0.05, rely=0.4, relwidth=0.5, relheight=0.2)
+button = Button(root, text="Generate Code", font='Courier 15 normal', command=code)
+button.place(relx=0.35, rely=0.9, relwidth=0.25, relheight=0.05)
+root.mainloop()
